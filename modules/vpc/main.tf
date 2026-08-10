32fe4e62d3c5b2b9ca7e971276303b0224d8dee2
ghuysu <daogiahuysu@gmail.com>
2026-08-10 17:24:05 +0700

    fix: format files

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,116 +6,116 @@ resource "aws_vpc" "this" {
   tags = merge(
     var.common_tags,
     {
-      Name        = "${var.project}-${var.environment}-vpc"
+      Name = "${var.project}-${var.environment}-vpc"
     }
   )
 }
 
 resource "aws_internet_gateway" "this" {
-    vpc_id = aws_vpc.this.id
+  vpc_id = aws_vpc.this.id
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-igw"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-igw"
+    }
+  )
 }
 
 resource "aws_subnet" "public" {
-    count                   = length(var.public_subnet_cidrs)
-    vpc_id                  = aws_vpc.this.id
-    cidr_block              = var.public_subnet_cidrs[count.index]
-    availability_zone       = var.availability_zones[count.index]
-    map_public_ip_on_launch = true
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.availability_zones[count.index]
+  map_public_ip_on_launch = true
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-public-subnet-${count.index + 1}"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-subnet-${count.index + 1}"
+    }
+  )
 }
 
 resource "aws_subnet" "private" {
-    count             = length(var.private_subnet_cidrs)
-    vpc_id            = aws_vpc.this.id
-    cidr_block        = var.private_subnet_cidrs[count.index]
-    availability_zone = var.availability_zones[count.index]
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = var.availability_zones[count.index]
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-private-subnet-${count.index + 1}"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-subnet-${count.index + 1}"
+    }
+  )
 }
 
 resource "aws_eip" "nat" {
-    count = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
-    domain = "vpc"
+  count  = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
+  domain = "vpc"
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-nat-eip-${count.index + 1}"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat-eip-${count.index + 1}"
+    }
+  )
 }
 
 resource "aws_nat_gateway" "this" {
-    count         = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
-    allocation_id = aws_eip.nat[count.index].id
-    subnet_id     = aws_subnet.public[count.index].id
+  count         = var.enable_nat_gateway ? length(var.public_subnet_cidrs) : 0
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-nat-gateway-${count.index + 1}"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-nat-gateway-${count.index + 1}"
+    }
+  )
 }
 
 resource "aws_route_table" "public" {
-    vpc_id = aws_vpc.this.id
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.this.id
-    }
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.this.id
+  }
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-public-rt"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-public-rt"
+    }
+  )
 }
 
 resource "aws_route_table_association" "public" {
-    count          = length(var.public_subnet_cidrs)
-    subnet_id      = aws_subnet.public[count.index].id
-    route_table_id = aws_route_table.public.id
+  count          = length(var.public_subnet_cidrs)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
 }
 
 
 resource "aws_route_table" "private" {
-    count  = var.enable_nat_gateway ? length(var.private_subnet_cidrs) : 0
-    vpc_id = aws_vpc.this.id
-    route {
-        cidr_block     = "0.0.0.0/0"
-        nat_gateway_id = aws_nat_gateway.this[count.index].id
-    }
+  count  = var.enable_nat_gateway ? length(var.private_subnet_cidrs) : 0
+  vpc_id = aws_vpc.this.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.this[count.index].id
+  }
 
-    tags = merge(
-        var.common_tags,
-        {
-            Name = "${var.project}-${var.environment}-private-rt-${count.index + 1}"
-        }
-    )
+  tags = merge(
+    var.common_tags,
+    {
+      Name = "${var.project}-${var.environment}-private-rt-${count.index + 1}"
+    }
+  )
 }
 
 resource "aws_route_table_association" "private" {
-    count          = var.enable_nat_gateway ? length(var.private_subnet_cidrs) : 0
-    subnet_id      = aws_subnet.private[count.index].id
-    route_table_id = aws_route_table.private[count.index].id
+  count          = var.enable_nat_gateway ? length(var.private_subnet_cidrs) : 0
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private[count.index].id
 }
